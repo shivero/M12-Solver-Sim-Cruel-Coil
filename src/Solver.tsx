4509eab9f3a37967376756ff2614eaefca503748
shivero <sh1veeer@gmail.com>
2026-07-16 22:41:44 +0200

    preformat

--- src/Solver.tsx
+++ src/Solver.tsx
@@ -1,16 +1,12 @@
-import { useState } from "react";
-import { SolverData } from "./solverdata";
-import {
-  type MechanicType,
-  AvailableMechanics,
-  type Roles as Role,
-} from "./solverdata.types";
+import { useState } from 'react';
+import { SolverData } from './solverdata';
+import { type MechanicType, AvailableMechanics, type Roles as Role } from './solverdata.types';
 export const Solver = () => {
   const [turn, setTurn] = useState<number>(0);
   const [mechanicResult, setMechanicPassed] = useState<boolean>(false);
   const [selectedMechanic, setSelectedMechanic] = useState<string>();
   const [playerRole, setRole] = useState<Role | null>(() => getRandomRole());
-  const buttonState = turn >= 5 ? "Reset" : "Next Turn";
+  const buttonState = turn >= 5 ? 'Reset' : 'Next Turn';
 
   const handleChangeTurn = () => {
     if (turn >= 5) {
@@ -18,27 +14,24 @@ export const Solver = () => {
       setRole(() => getRandomRole());
       return;
     }
-    setTurn((prev) => prev + 1);
-    setSelectedMechanic("");
+    setTurn(prev => prev + 1);
+    setSelectedMechanic('');
   };
-  const currentTurn = SolverData.find((w) => w.turn === turn);
+  const currentTurn = SolverData.find(w => w.turn === turn);
   const pickOption = (selectedOption: MechanicType) => {
-    console.log("picked", selectedOption);
+    // console.log("picked", selectedOption);
     setSelectedMechanic(selectedOption);
     if (playerRole) {
-      const playerParticipateInCurrentTurn =
-        currentTurn?.solver.participants.includes(playerRole);
-      console.log(playerParticipateInCurrentTurn);
-      const nothingIsSelected = selectedOption === "nothing";
+      const playerParticipateInCurrentTurn = currentTurn?.solver.participants.includes(playerRole);
+      // console.log(playerParticipateInCurrentTurn);
+      const nothingIsSelected = selectedOption === 'nothing';
       if (!playerParticipateInCurrentTurn && nothingIsSelected) {
         setMechanicPassed(true);
         return;
       }
       if (playerParticipateInCurrentTurn) {
-        const mechanicForPlayer = currentTurn?.solver.actions.find(
-          (action) => action.role === playerRole,
-        );
-        console.log("playerParticipateInThisTurn", mechanicForPlayer);
+        const mechanicForPlayer = currentTurn?.solver.actions.find(action => action.role === playerRole);
+        // console.log("playerParticipateInThisTurn", mechanicForPlayer);
         if (playerParticipateInCurrentTurn && nothingIsSelected) {
           setMechanicPassed(false);
         }
@@ -60,16 +53,10 @@ export const Solver = () => {
       <h3>Assigned role: {playerRole} </h3>
       {turn > 0 ? (
         <div>
-          <div>Participants: {participants.join(", ")} </div>
-          {selectedMechanic ? (
-            mechanicResult ? (
-              <div>✅ Great job</div>
-            ) : (
-              <div>❌ Wrong</div>
-            )
-          ) : null}
+          <div>Participants: {participants.join(', ')} </div>
+          {selectedMechanic ? mechanicResult ? <div>✅ Great job</div> : <div>❌ Wrong</div> : null}
           <strong>Actions:</strong>
-          {actionsToTake.map((w) => (
+          {actionsToTake.map(w => (
             <div key={w} className="options" onClick={() => pickOption(w)}>
               {w}
             </div>
@@ -83,6 +70,6 @@ export const Solver = () => {
 const getRandomRole = () => {
   const randomRoleNum = Math.floor(Math.random() * (4 - 1 + 1) + 1);
   const randomRoleSymbol = Math.floor(Math.random() * (2 - 1 + 1) + 1);
-  const symbol = randomRoleSymbol > 1 ? "A" : "B";
+  const symbol = randomRoleSymbol > 1 ? 'A' : 'B';
   return `${symbol}${randomRoleNum}` as Role;
 };
